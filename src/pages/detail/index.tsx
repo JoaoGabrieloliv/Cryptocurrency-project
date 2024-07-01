cd819ba5eb378b0b5f9f35a2ee2382b3ba35c432
JOAO GABRIEL OLIVEIRA SOUZA LIMA SALES <joao.sales@demprel.local>
2024-07-01 12:56:58 -0300

    Joao

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,9 @@
+export function Detail() {
+    return <div>
+        <h1>Pagina Detalhe da moeda</h1>
+      
+    </div>;
+  }
+  
+  
+  
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,7 @@
+export function Notfound() {
+  return (
+    <div>
+      <h1>Pagina 404, Esta pagina não existe</h1>
+    </div>
+  );
+}
