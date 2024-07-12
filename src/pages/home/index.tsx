ba609a8dcde4ebdbaab9754d894c997cc3b34b4f
JOAO GABRIEL OLIVEIRA SOUZA LIMA SALES <joao.sales@demprel.local>
2024-07-12 15:35:49 -0300

    Joao ATUALIZAÇAO

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,79 @@
+import { useState, FormEvent, useEffect } from "react";
 import styles from "./home.module.css";
 import { BsSearch } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+interface CoinProps {
+  id: string;
+  rank: string;
+  symbol: string;
+  name: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  priceUsd: string;
+  changePercent24H: string;
+  vwap24Hr: string;
+  explorer: string;
+}
+
+interface DataProp {
+  data: CoinProps[];
+}
 
 export function Home() {
+  const [input, setInput] = useState("");
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    fetch("https://api.coincap.io/v2/assets?limit=10&offset=0")
+      .then((response) => response.json())
+      .then((data: DataProp) => {
+        const coinsData = data.data;
+
+        const price = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        });
+
+        const formatedResult = coinsData.map((item) => {
+          const formated = {
+            ...item,
+            formatedPrice: price.format(Number(item.priceUsd)),
+            formatedMarket: price.format(Number(item.marketCapUsd)),
+          };
+
+          return formated;
+        });
+      });
+  }
+
+  function handleGetMore() {}
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    navigate(`/detail/${input}`);
+  }
+
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
-        <input type="text" placeholder="Digite o nome da moeda..." />
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o nome da moeda..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button type="submit">
           <BsSearch size={30} color="#FFF" />
         </button>
@@ -45,12 +112,16 @@ export function Home() {
               2B
             </td>
 
-            <td className={styles.tdLabel} data-Label="Mudança 24h">
+            <td className={styles.tdProfit} data-Label="Mudança 24h">
               <span>1.20</span>
             </td>
           </tr>
         </tbody>
       </table>
+
+      <button className={styles.buttonMore} onClick={handleGetMore}>
+        Carregar mais
+      </button>
     </main>
   );
 }
